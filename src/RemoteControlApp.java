69a445c28ca88a46a23f6350c283aac3e23cc9d8
Neal Savant <nealrsavant@gmail.com>
2019-11-08 16:43:35 -0700

    Initial entrys and TODO

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,9 +1,23 @@
+//AUTHOR : Neal Savant
 
 
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
+		
+		System.out.println("To begin, turn on the remote control: ");
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+	
+	//TODO: Add pressButton() Method
+	
+	//TODO: Add turnOnButton() Method
+	
+	public static void printMenu() {
 		
 	}
 
