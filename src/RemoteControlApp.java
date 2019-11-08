ee23ef5385dd7ec0f560bdc3bcb62df0f53201c0
Neal Savant <nealrsavant@gmail.com>
2019-11-08 16:47:24 -0700

    adjusted spacing

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,25 +1,24 @@
 //AUTHOR : Neal Savant
 
-
 import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("To begin, turn on the remote control: ");
 		Scanner sc = new Scanner(System.in);
-		
+
 		String input = "";
-		
+
 	}
-	
-	//TODO: Add pressButton() Method
-	
-	//TODO: Add turnOnButton() Method
-	
+
+	// TODO: Add pressButton() Method
+
+	// TODO: Add turnOnButton() Method
+
 	public static void printMenu() {
-		
+
 	}
 
 }
