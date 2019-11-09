3df9f00c55b1469f859484ef5f0c2c19cbc3c469
Neal Savant <nealrsavant@gmail.com>
2019-11-08 17:50:37 -0700

    Almost finished lol

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -11,37 +11,83 @@ public class RemoteControlApp {
 		System.out.print("To begin, turn on the remote control (on): ");
 		// import scanner
 		Scanner sc = new Scanner(System.in);
-		// receive user string
 		String input = sc.next();
 
-		if (input.equalsIgnoreCase("on")) {
-			online = true;
-			pressButton(online);
+		// wait for the user to turn on the machine
+		while (online == false) {
+			if (input.equalsIgnoreCase("on")) {
+				online = true;
+				powerButton(online);
+			} else {
+				System.out.println("Please turn the remote control on.");
+				input = sc.next();
+			}
+		}
+
+		// user has turned on the program
+
+		while (online == true) {
+			System.out.println("PLEASE PRESS A BUTTON 1-9");
+			input = sc.next();
+			pressButton(input);
 		}
 
 		sc.close();
 
 	}
 
-	// TODO: Add pressButton() Method
-	public static boolean pressButton(boolean status) {
+	public static boolean powerButton(boolean status) {
 		boolean action = status;
 		if (status == true) {
 			System.out.println("TURNING ON...");
-			System.out.println("PLEASE PRESS A BUTTON 1-9");
+
 			return action;
-		}
-		else if(status == false) {
+		} else if (status == false) {
 			System.out.println("TURNING OFF...");
 			return action;
 		}
 		return action;
 	}
 
-	// TODO: Add turnOnButton() Method
+	public static String pressButton(String choice) {
+		String action = choice;
 
-	public static void printMenu() {
-
+		// switch statement with buttons
+		switch (choice) {
+		case "1":
+			System.out.println("Beep Boop!");
+			return action;
+		case "2":
+			System.out.println("Boop Bop!");
+			return action;
+		case "3":
+			System.out.println("Wzzzzzding bang!");
+			return action;
+		case "4":
+			System.out.println("BEEP BEEP BEEP!");
+			return action;
+		case "5":
+			System.out.println("Brrriiiing!");
+			return action;
+		case "6":
+			System.out.println("Shk-Shk-Shk-Shk!");
+			return action;
+		case "7":
+			System.out.println("Beep Boop!");
+			return action;
+		case "8":
+			System.out.println("Beep Boop!");
+			return action;
+		case "9":
+			System.out.println("Beep Boop!");
+			return action;
+		case "off":
+			powerButton(false);
+			return action;
+		default:
+			System.out.println("Command not recognized");
+			return action;
+		}
 	}
 
 }
